26f251318b684ff8a264f022cda6ef50c4cb0b03
sophkees1 <sophiekeesing01@gmail.com>
2023-08-25 16:07:26 +0200

    sophie adding admin main menu, author menu, and log menu. also added some changes to initial user main and user account menus

--- src/menu.py
+++ src/menu.py
@@ -2,27 +2,73 @@ class Menu():
 
     @staticmethod
     def display_main_user_menu():
-        print("""
-              
+        print("""         
               MAIN MENU
               
               1. Account menu
               2. Book menu
               3. Borrowed book menu
               4. Logs menu
-              5. Log out
-              6. Exit
+              88. Log out
+              99. Exit App
               """)
 
     @staticmethod
     def display_user_acc_menu():
         print("""
-              Please pick an option:
+              USER ACCOUNT MENU:
+              
               1. Change username
               2. Change password
               3. Change email
               4. Change phone number
               5. Change recovery question
               6. Go back to previous menu
-              7. Go to main menu
+              77. Go back to User Main Menu
+              88. Log Out
+              99. Exit App
+              
               """)
+
+    @staticmethod
+    def display_admin_main_menu():
+        print("""
+              ADMIN MAIN MENU:
+              
+              1. Account Menu
+              2. Author Menu
+              3. Book Copy Menu
+              4. Borrowed Book Menu
+              5. Log Menu
+              88. Log Out
+              99. Exit App
+              
+              """)
+        
+    @staticmethod
+    def display_admin_author_menu():
+        print("""
+              ADMIN AUTHOR MENU:
+              
+              1. Add Author
+              2. Modify Author
+              3. Delete Author
+              4. Search by Author
+              77. Go back to Admin Main Menu
+              88. Log Out
+              99. Exit App
+              
+              """)
+        
+    @staticmethod
+    def display_admin_log_menu():
+        print("""
+              ADMIN LOG MENU:
+              
+              1. Report Bugs
+              2. Generate Bug Report
+              77. Go back to Admin Main Menu
+              88. Log Out
+              99. Exit App
+              
+              """)  
