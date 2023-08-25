f7d130f187ea8a7c28d88dea8d6396c71c6b299a
Jaime <jay4000@hotmail.co.uk>
2023-08-25 16:11:24 +0200

    added user book menu & user borrowed book menu

--- src/menu.py
+++ src/menu.py
@@ -1,8 +1,8 @@
-class Menu():
-
+class Menu:
     @staticmethod
     def display_main_user_menu():
-        print("""
+        print(
+            """
               
               MAIN MENU
               
@@ -12,11 +12,13 @@ class Menu():
               4. Logs menu
               5. Log out
               6. Exit
-              """)
+              """
+        )
 
     @staticmethod
     def display_user_acc_menu():
-        print("""
+        print(
+            """
               Please pick an option:
               1. Change username
               2. Change password
@@ -25,4 +27,37 @@ class Menu():
               5. Change recovery question
               6. Go back to previous menu
               7. Go to main menu
-              """)
+              """
+        )
+
+    @staticmethod
+    def user_book_menu():
+        print(
+            """
+            
+            BOOK MENU
+            
+            1. Book library display
+            2. Book(s) search by ISBN/author/title/genre
+            3. Check book location
+            4. Borrow book
+            5. Return book
+            6. Go back to previous Menu
+            77. Go to Main Menu
+            
+            """
+        )
+
+    @staticmethod
+    def user_borrowed_book_menu():
+        print(
+            """
+            BORROWED BOOKS MENU
+            
+            1. What books is the User borrowing?
+            2. Display due dates
+            3. Go back to previous Menu
+            77. Go back to Main Menu
+            
+            """
+        )
